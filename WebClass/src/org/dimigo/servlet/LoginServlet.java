a8fa39b4a38cb333fd16a014b0d3523985d53ff1
eslerkang <eslerkang@gmail.com>
2017-09-27 21:31:19 +0900

    k

--- WebClass/src/org/dimigo/servlet/LoginServlet.java
+++ WebClass/src/org/dimigo/servlet/LoginServlet.java
@@ -3,12 +3,15 @@ package org.dimigo.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.dimigo.vo.UserVo;
 import org.json.simple.JSONObject;
 
 import com.google.gson.Gson;
@@ -34,7 +37,9 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd = request.getRequestDispatcher("jsp/login.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
@@ -42,6 +47,42 @@ public class LoginServlet extends HttpServlet {
 	 */
 	@SuppressWarnings("unchecked")
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		System.out.printf("id : %s, pwd : %s\n",id,pw);
+		
+		Gson gson = new Gson();
+		JsonObject json = new JsonObject();
+		json.addProperty("id",id);
+		String j = gson.toJson(json);
+		System.out.println(j);
+		out.write(j);
+		boolean result = true;
+		
+		if(result) {
+			HttpSession session = request.getSession();
+			UserVo user = new UserVo();
+			user.setId(id);
+			user.setName("È«±æµ¿");
+			user.setNick("ÀÇÀû");
+			session.setAttribute("user", user);
+			RequestDispatcher rd = request.getRequestDispatcher("jsp/home.jsp");
+			rd.forward(request, response);
+			
+		}
+		else {
+			request.setAttribute("msg", "error");
+			RequestDispatcher rd = request.getRequestDispatcher("jsp/login.jsp");
+			rd.forward(request, response);
+		}
+		out.close();
+		
+	}
+	protected void doPost2(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setContentType("application/json;charset=utf-8");
 		PrintWriter out = response.getWriter();
@@ -70,5 +111,4 @@ public class LoginServlet extends HttpServlet {
 		out.close();
 		
 	}
-
 }
---
+++ WebClass/src/org/dimigo/vo/UserVo.java
@@ -0,0 +1,25 @@
+package org.dimigo.vo;
+
+public class UserVo {
+	private String id;
+	private String name;
+	private String nick;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+}
